3b5799d3d89370745a40807a658b26a637b34a07
truongvi <nltruongvi@gmail.com>
2021-01-20 11:20:56 +0700

    Init project

---
+++ containers/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='flex space-x-4'>
+            <div className="w-4/12 bg-gray-50">1</div>
+            <div className="w-8/12 bg-gray-50">2</div>
+        </header>
+    );
+};
+
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // purge: [],
+  purge: ['./pages/**/*.js', './pages/**/*.jsx', './containers/**/**.[tsx]', './pages/**/*.tsx', './pages/**/*.ts', './components/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
